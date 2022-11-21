d2b3ebc75928e7efa08c6b73a024f7ca689240e4
Hasan Atki <djamblers@gmail.com>
2022-11-21 00:08:54 +0000

    deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-atki-codestar.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['http://django-atki-codestar.herokuapp.com/', 'localhost']
 
 
 # Application definition
